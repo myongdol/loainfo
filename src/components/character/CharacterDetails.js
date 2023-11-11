b16f7e8765b147ef11bdb42b245479016a898f56
yong seock <myongdol132@gmail.com>
2023-11-11 22:15:32 +0900

    Feat: Character.js 로딩상태 및 에러핸들링 추가
    - isLoading상태를 이용한 캐릭터의 정보를 가져오는 동안 로딩 상태 표시
    - 존재하지 않는 캐릭명을 입력할 경우 사용자에게 오류 메시지 출력

--- src/components/character/CharacterDetails.js
+++ src/components/character/CharacterDetails.js
@@ -16,33 +16,48 @@ const api = axios.create({
 
 function CharacterDetails() {
   const { characterName } = useParams();
-    const [siblings, setSiblings] = useState([]);
+  const [siblings, setSiblings] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+      setIsLoading(true);
+
       if (characterName) {
         api.get(`/characters/${encodeURIComponent(characterName)}/siblings`)
           .then(response => {
             // API가 배열을 반환하지 않는 경우를 대비하여 체크
             setSiblings(Array.isArray(response.data) ? response.data : []);
+            setIsLoading(false);
           })
           .catch(error => {
             console.error('원정대 정보를 불러올 수 없습니다.', error);
             alert('존재하지 않는 캐릭터명 입니다.')
-            setSiblings([]); // 오류 발생 시 상태를 빈 배열로 재설정
+            setIsLoading(false);
+            setSiblings(null); // 오류 발생 시 상태를 빈 배열로 재설정
           });
+      } else {
+        setIsLoading(false);
       }
     }, [characterName]);
+
+    if (isLoading) {
+      return <div>로딩 중...</div>;
+    }
 
     
     return (
       <div>
-          <ul>
-            {siblings.map((sibling, index) => (
-              <li key={index}>
-                {sibling.CharacterName} - {sibling.CharacterLevel}
-              </li>
-            ))}
-          </ul>
+         {siblings.length > 0 ? (
+        <ul>
+          {siblings.map((sibling, index) => (
+            <li key={index}>
+              {sibling.CharacterName} - {sibling.CharacterLevel}
+            </li>
+          ))}
+        </ul>
+        ) : (
+          <div>캐릭터 정보가 없습니다.</div>
+        )}
       </div>
     );
   }
