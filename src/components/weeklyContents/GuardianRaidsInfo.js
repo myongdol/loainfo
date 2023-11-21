4534d170b3b321c9e44f60cd6dd39e1d7a6b4584
yong seock <myongdol132@gmail.com>
2023-11-21 19:15:17 +0900

    Refactor: GuardianRadisInfo 컴포넌트에 StyledButton 적용
    - 컴포넌트내에 직접 작성되어 있던 StyledButton을 별도로 분리된
      UI컴포넌트인 StyledButton으로 대체

--- src/components/weeklyContents/GuardianRaidsInfo.js
+++ src/components/weeklyContents/GuardianRaidsInfo.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import useApi from "../../util/useApi";
 import GuardianRewardModal from "./GuardianRewardModal";
+import { StyledButton } from "../UI/StyeldButton";
 
 
 const GuardianRaidsInfo = () => {
@@ -74,34 +75,4 @@ const GuardianImg = styled.img`
   max-width: 100%;
   height: auto;
   object-fit: cover;
-`;
-
-const StyledButton = styled.button`
-  box-sizing: border-box;
-  appearance: none;
-  background-color: transparent;
-  border: 2px solid ${props => props.theme.colors.softBlueGrey};
-  border-radius: 0.6em;
-  color: ${props => props.theme.colors.softBlueGrey}; 
-  cursor: pointer;
-  display: flex;
-  align-self: center;
-  font-size: 1rem;
-  font-weight: 700;
-  line-height: 1;
-  margin: 10px;
-  padding: 1.2em 2.8em;
-  text-decoration: none;
-  text-align: center;
-  text-transform: uppercase;
-  font-family: 'Montserrat', sans-serif;
-
-  &:hover,
-  &:focus {
-    color: #fff;
-    outline: 0;
-    box-shadow: 0 0 40px 40px ${props => props.theme.colors.highlight} inset;
-  }
-
-  transition: box-shadow 300ms ease-in-out, color 300ms ease-in-out;
 `;
