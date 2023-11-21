49c6148fcda6089df8e0ffbfee34a0dc45eb90a0
yong seock <myongdol132@gmail.com>
2023-11-22 01:54:02 +0900

    Feat: Rowen Update(UI 및 기능개선)
    - 로웬 이벤트의 시작 시간을 보다 명확하게 표시하기 위해 formatDateTime
      함수 추가, 기존의 시간 문자열을 사용자 친화적인 형태로 반환
    - 이벤트 아이템을 표시하는 레이아웃을 EventContainer, EventItem
      스타일컴포넌트를 사용하여 개선, 보다 직관적인 UI와 반응형 디자인

--- src/components/weeklyContents/calendarEvents/Rowen.js
+++ src/components/weeklyContents/calendarEvents/Rowen.js
@@ -12,26 +12,31 @@ const Rowen = ({ events }) => {
     setModalOpen(true);
   };
 
+  const formatDateTime = (dateTimeStr) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+    return new Date(dateTimeStr).toLocaleString('ko-KR', options);
+  };
+
 
   return (
     <Section>
       <h2>로웬 이벤트</h2>
-      
+      <EventContainer>
       {events.map((event, index) => (
-        <div key={index}>
+        <EventItem key={index}>
           <h3>{event.ContentsName}</h3>
           <p>위치: {event.Location}</p>
           <ContentsIcon src={event.ContentsIcon}/>
-          <p>시작 시간:</p>
           <ul>
+          <p>로웬 일정</p>
             {event.StartTimes.map((startTime, timeIndex) => (
-              <li key={timeIndex}>{startTime}</li>
+              <li key={timeIndex}>{formatDateTime(startTime)}</li>
             ))}
           </ul>
           <p>최소 아이템 레벨: {event.MinItemLevel}</p>
-        </div>
+        </EventItem>
       ))}
-
+    </EventContainer>
       <StyledButton onClick={handleOpenModal}>보상 보기</StyledButton>
 
       {modalOpen && (
@@ -56,10 +61,33 @@ const Section = styled.section`
   color: ${(props) => props.theme.colors.text};
   background-color: ${(props) => props.theme.colors.dark};
   margin: auto;
-  flex-direction: row;
+  display: flex;
+  flex-direction: column;
 `;
 
 const ContentsIcon = styled.img`
  width: 120px;
  height: 120px;
 `
+
+const EventContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 20px;
+`
+
+const EventItem = styled.div`
+  flex-basis: calc(25% - 20px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    flex-basis: calc(50% - 20px);
+  }
+
+  @media (max-width: 480px) {
+    flex-basis: 100%;
+  }
+`;
