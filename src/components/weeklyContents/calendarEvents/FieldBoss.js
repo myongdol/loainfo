90109d2dc83533ee52e5ddd47413c57e747fb893
yong seock <myongdol132@gmail.com>
2023-11-27 21:44:57 +0900

    Feat: 필드보스 컴포넌트 UI개선 및 위치모달 추가
    - 카오스게이트와 동일한 스타일링으로 UI 일관성 유지
    - 등장지역이 상당히 많으므로 LocationModal을 재사용하여 위치정보를
      모달형식으로 제공

--- src/components/weeklyContents/calendarEvents/FieldBoss.js
+++ src/components/weeklyContents/calendarEvents/FieldBoss.js
@@ -3,15 +3,20 @@ import styled from "styled-components";
 import useRemainingTimer from "../../../util/useRemainingTimer";
 import { StyledButton } from "../../UI/StyeldButton";
 import FieldBossRewardModal from "../../UI/Modals/FieldBossRewardModal";
-
+import LocationModal from "../../UI/Modals/LocationModal";
 
 
 const FieldBoss = ({ events }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const remainingTimes = useRemainingTimer(events);
+  const [locationModalOpen, setLocationModalOpen] = useState(false);
 
   const handleOpenModal = () => {
     setModalOpen(true);
+  };
+
+  const handleLocationModalOpen = () => {
+    setLocationModalOpen(true);
   };
   
   const nextStartTime = remainingTimes.length > 0 ? remainingTimes[0].time : '정보 없음';
@@ -19,19 +24,31 @@ const FieldBoss = ({ events }) => {
 
   return (
     <Section>
-      <h2>필드보스</h2>
-      <ContentsIcon src={events[0].ContentsIcon} />
-      <div>
-        <p>등장 지역: {events.map(event => event.Location).join(', ')}</p>
-        <p>시작 시간: {nextStartTime}</p>
-        <StyledButton onClick={handleOpenModal}>보스별 보상 보기</StyledButton>
-      </div>
+      <Title>필드보스</Title>
+      <EventItemContainer>
+        <ContentsIcon src={events[0].ContentsIcon} />
+        <EventDetails>
+          <EventInfo> 시작 시간
+            <StartTime>{nextStartTime}</StartTime>
+          </EventInfo>
+          <StyeldBtn onClick={handleLocationModalOpen}>등장지역 보기</StyeldBtn>
+          <StyeldBtn onClick={handleOpenModal}>보스별 보상 보기</StyeldBtn>
+        </EventDetails>
+      </EventItemContainer>
 
       {modalOpen && (
         <FieldBossRewardModal
           isOpen={modalOpen}
           onClose={() => setModalOpen(false)}
           events={events}
+        />
+      )}
+
+      {locationModalOpen && (
+        <LocationModal 
+          isOpen={locationModalOpen}
+          onClose={() => setLocationModalOpen(false)}
+          locations={events.map(event => event.Location).join(', ')}
         />
       )}
     </Section>
@@ -46,7 +63,45 @@ const Section = styled.section`
   padding: 10px;
 `;
 
+const Title = styled.h2`
+  padding-bottom: 30px;
+`;
+
+const EventItemContainer = styled.div`
+  min-height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
 const ContentsIcon = styled.img`
   width: 120px;
   height: 120px;
+`;
+
+
+const EventDetails = styled.div`
+  flex-grow: 1;
+`;
+
+const EventInfo = styled.p`
+  margin: 5px 0; 
+  white-space: pre-wrap;
+`;
+
+const StartTime = styled.span`
+  display: block;
+  color: ${(props) => props.theme.colors.orange}
+`;
+
+const StyeldBtn = styled(StyledButton)`
+  width: 100%;
+  margin-top: 15px;
+  text-align: center;
+  justify-content: center;
+  display: flex;
+
+  @media (max-width: 480px) {
+    font-size: 1.2em;
+  }
 `;
