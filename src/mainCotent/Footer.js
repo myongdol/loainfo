9f518c457c5db425712bc5d907779a57afe718e3
yong seock <myongdol132@gmail.com>
2023-12-01 23:56:48 +0900

    Feat: Footer컴포넌트 업데이트
    - 저작권 정보 및 연락처 정보 추가
    - LostArk의 공식 API 사용에 대한 명시
    - Github링크와 이메일 주소 추가로 개발자 연락처 제공

--- src/mainCotent/Footer.js
+++ src/mainCotent/Footer.js
@@ -1,18 +1,75 @@
 import styled from "styled-components";
+import { FaGithub } from 'react-icons/fa';
+
 
 function Footer() {
     return (
         <StyledFooter>
-            저작권 정보, 연락처, 깃헙 등 
+            <Copyright>
+                <p>이 사이트는 Lost Ark의 공식 API를 사용합니다.</p>
+                <p>
+                    모든 관련 콘텐츠의 저작권은 
+                    <a href="https://lostark.game.onstove.com/Main" target="_blank" rel="noopener noreferrer"> Lost Ark</a>에 있습니다.
+                </p>
+            </Copyright>
+            <FooterContent>
+                <a href="https://github.com/myongdol" target="_blank" rel="noopener noreferrer">
+                    <FaGithub /> Github
+                </a>
+                <FooterEmail href="mailto:myongdol132@gmail.com">myongdol132@gmail.com</FooterEmail>
+            </FooterContent>
         </StyledFooter>
     )
 };
 
 export default Footer;
 
+
+
 const StyledFooter = styled.footer`
   background-color: #333;
   color: white;
   padding: 1rem;
   text-align: center;
+`;
+
+const Copyright = styled.div`
+    margin: 0 auto;
+
+    p {
+        margin: 5px 0;
+    }
+
+    a {
+        color: #007bff;
+        text-decoration: none;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+`;
+
+const FooterContent = styled.div`
+  a {
+    color: white;
+    text-decoration: none;
+    margin-right: 15px;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    &:hover {
+      color: #ccc;
+    }
+  }
+`;
+
+const FooterEmail = styled.a`
+  color: white;
+  text-decoration: none;
+
+  &:hover {
+    color: #ccc;
+  }
 `;
