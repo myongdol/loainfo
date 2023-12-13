41a9c3ed7968377af20675eeeb588ea3e5daf523
yong seock <myongdol132@gmail.com>
2023-12-13 21:43:55 +0900

    Style: QualityOverlay 컴포넌트 품질 막대 색상을 그라데이션으로 변경
    - 기존 임시색상에서 게임내 품질 색상을 참고하여 그라데이션으로 변경
    - getQualityColor함수를 수정하여 각 품질 구간별로 다른 그라데이션 색상을
      반환

--- src/components/characterPageContents/Details/QualityOverlay.js
+++ src/components/characterPageContents/Details/QualityOverlay.js
@@ -17,15 +17,25 @@ const QualityOverlay = ({ qualityValue }) => {
   );
 };
 
-// 임시색상
+
 function getQualityColor(qualityValue) {
-    if (qualityValue >= 100) return 'gold';
-    if (qualityValue >= 90) return 'purple';
-    if (qualityValue >= 70) return 'blue';
-    if (qualityValue >= 30) return 'green';
-    if (qualityValue >= 10) return 'yellow';
-    return 'red';
+  if (qualityValue >= 100) {
+    return 'linear-gradient(to right, #E68F34, #FF9231, #FFC739)';
   }
+  if (qualityValue >= 90) {
+    return 'linear-gradient(to right, #7A1890, #9B0CD4, #B50CFF)';
+  }
+  if (qualityValue >= 70) {
+    return 'linear-gradient(to right, #0A5EA4, #10A6EF, #10C7FF)';
+  }
+  if (qualityValue >= 30) {
+    return 'linear-gradient(to right, #397108, #B2FC10, #CFFF10)'
+  }
+  if (qualityValue >= 10) {
+    return 'linear-gradient(to right, #FAE410, #FFEE0C, #FFF90D)'
+  };
+  return 'linear-gradient(to right, #FF521B, #FF6518)';
+}
 
 export default QualityOverlay;
 
@@ -42,13 +52,13 @@ const QualityBarContainer = styled.div`
   position: absolute;
   bottom: 0;
   width: 100%;
-  height: 12px;
+  height: 14px;
   background-color: #FFFFFF;
   border-radius: 0 0 5px 5px;
 `;
 
 const QualityBarFilled = styled.div`
-  background-color: ${props => props.backgroundColor};
+  background: ${props => props.backgroundColor};; 
   width: ${props => props.width}%;
   height: 100%;
   transition: width 0.5s ease-in-out;
@@ -65,7 +75,7 @@ const StyledQualityValue = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
   color: #ffffff;
-  font-size: 0.9em;
+  font-size: 1em;
   text-shadow: 0 0 3px #000;
   z-index: 1;
 `;
