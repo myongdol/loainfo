9bc63c0bd46e69ce65f294cdc39a1310bd73b9f9
yong seock <myongdol132@gmail.com>
2023-12-14 21:52:48 +0900

    Design: CharacterTabs에 StyledButton 적용
    - ButtonsContainer를 추가하여 버튼을 정렬하고 공간을 균등하게 분배
    - StyledButton를 재사용하고 flex: 1 스타일을 적용하여 각 버튼이 컨테이너
      공간을 균등하게 차지하도록 조정

--- src/components/characterPageContents/ChracterPageTabs.js
+++ src/components/characterPageContents/ChracterPageTabs.js
@@ -2,16 +2,17 @@ import React, { useState } from 'react';
 import EquipmentTab from './Tabs/EquipmentTab';
 import OwnedCharactersTab from './Tabs/OwnedCharactersTab';
 import styled from 'styled-components';
+import { StyledButton } from '../UI/StyeldButton';
 
 const CharacterTabs = ({ equipment, siblings }) => {
   const [activeTab, setActiveTab] = useState('equipment');
 
   return (
     <StyledTabs>
-      <div>
-        <button onClick={() => setActiveTab('equipment')}>장비</button>
-        <button onClick={() => setActiveTab('characters')}>보유 캐릭터</button>
-      </div>
+      <ButtonsContainer>
+        <StyledButtons onClick={() => setActiveTab('equipment')}>장비</StyledButtons>
+        <StyledButtons onClick={() => setActiveTab('characters')}>보유 캐릭터</StyledButtons>
+      </ButtonsContainer>
       {activeTab === 'equipment' && <EquipmentTab equipments={equipment} />}
       {activeTab === 'characters' && <OwnedCharactersTab siblings={siblings} />}
     </StyledTabs>
@@ -24,4 +25,14 @@ export default CharacterTabs;
 
 const StyledTabs = styled.div`
   background-color: ${props => props.theme.colors.background};
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const StyledButtons = styled(StyledButton)`
+  flex: 1;
 `;
