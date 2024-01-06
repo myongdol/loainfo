61e1bc2571ebe68365a8dacc8fc40472d8350551
myongdol <myongdol132@gmail.com>
2024-01-06 23:02:13 +0900

    Design: GuildPage 검색 바 디자인
    - 기존 단순 입력 필드에서 스타일링된 검색 바로 변경

--- src/pages/GuildPage.js
+++ src/pages/GuildPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import GuildRankings from '../components/Guild/GuildRankings';
 import styled from 'styled-components';
+import { MdOutlineSearch } from 'react-icons/md';
 
 
 
@@ -18,10 +19,10 @@ const GuildPage = () => {
 
   return (
     <GuildPageContainer>
-      <form onSubmit={handleSearchSubmit}>
-        <input type="text" value={searchTerm} onChange={handleSearchChange} placeholder="길드 검색" />
-        <button type="submit">Search</button>
-      </form>
+      <SearchBarContainer  onSubmit={handleSearchSubmit}>
+        <SearchInput type="text" value={searchTerm} onChange={handleSearchChange} placeholder="길드 검색" />
+        <StyledSearchIcon size="20px" onClick={handleSearchSubmit} />
+      </SearchBarContainer>
       <GuildRankings />
     </GuildPageContainer>
   );
@@ -34,4 +35,41 @@ export default GuildPage;
 const GuildPageContainer = styled.div`
   background-color: ${(props) => props.theme.colors.dark};
   color: white;
+`;
+
+const SearchBarContainer = styled.form`
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 2.5rem;
+  padding: 0 0.75rem;
+  border-radius: 0.375rem;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+  background-color: #E5E7EB;
+  border: 1px solid #E5E7EB;
+  max-width: 60%;
+  width: 450px;
+  margin: 10px auto 0;
+`;
+
+const SearchInput = styled.input`
+  flex: 1;
+  border: none;
+  padding: 0.5rem;
+  padding-right: 2rem;
+  font-size: 1rem;
+  background-color: #E5E7EB;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const StyledSearchIcon = styled(MdOutlineSearch)`
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  color: #000000;
+  cursor: pointer;
 `;
